e6e12142d958f6a243c720409a7087f18916d0e1
Radwan <145333687+larqsine@users.noreply.github.com>
2024-03-14 11:09:20 +0100

    Set up scene

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+public class Main extends Application {
+
+    public void start(Stage primaryStage) throws Exception {
+
+
+        Parent root = FXMLLoader.load(getClass().getResource(""));
+        primaryStage.setTitle("ImageViewer beta");
+        primaryStage.show();
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(args);
+
+
     }
 }
